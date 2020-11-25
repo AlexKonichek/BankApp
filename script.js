948573c2e63110c30af7154665c4930c29df9367
Alex Konichek <Konichekalex@gmail.com>
2020-11-25 14:18:17 +0200

    fixed displaing some numbers by rounding

--- script.js
+++ script.js
@@ -78,7 +78,7 @@ const displayMovements = function (movements, sort = false) {
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__value">${mov}€</div>
+        <div class="movements__value">${mov.toFixed(2)}€</div>
       </div>
     `;
 
@@ -89,19 +89,19 @@ const displayMovements = function (movements, sort = false) {
 const calcDisplayBalance = function (acc) {
   console.log(acc.movements)
  acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
- labelBalance.textContent = `${acc.balance}€`;
+ labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
 };
 
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}€`;
+  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
 
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out)}€`;
+  labelSumOut.textContent = `${Math.abs(out.toFixed(2))}€`;
 
   const interest = acc.movements
     .filter(mov => mov > 0)
@@ -111,7 +111,7 @@ const calcDisplaySummary = function (acc) {
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest}€`;
+  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
 };
 const createUsernames = function (accs) {
   accs.forEach(function (acc) {
@@ -179,6 +179,26 @@ btnTransfer.addEventListener('click', function(e){
   else{console.log('transfer is  NOT valid')}
 })
 
+//requesting a loan
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault()
+  const amount = Math.floor(inputLoanAmount.value)
+//you should add some varification on inputs
+  if (amount > 0 && currentAcount.movements.some(mov => mov >= amount * 0.1)) {
+   console.log('valid loan')
+    //add the movement to current account
+    currentAcount.movements.push(amount);
+    //update UI
+    updateUI(currentAcount);
+    inputLoanAmount.value = ''
+  }
+  else{inputLoanAmount.value = ''
+  
+    
+    }
+})
+
+
 //close account  feature
 btnClose.addEventListener('click', function (e) {
   e.preventDefault()
@@ -198,4 +218,6 @@ btnClose.addEventListener('click', function (e) {
 
   
 })
+
+
 
